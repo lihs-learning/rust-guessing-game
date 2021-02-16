6c3c6c49c40c8cb73ba33e31c372646bdc5f7e9a
LiHS <lihsai0@gmail.com>
2021-02-16 18:27:06 +0800

    feat: echo stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("猜数字！");
+
+    println!("请输入你猜的数字。");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("没有读到任何内容");
+
+    println!("你猜的数字是：{}", guess);
 }
