6d2d93f189307cfbf95664e3374a7ef130e3ffaf
LiHS <lihsai0@gmail.com>
2021-02-17 20:52:31 +0800

    feat: finish demo

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,31 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101); // 左闭右开
 
-    println!("秘密数字为：{}", secret_number);
+    // println!("秘密数字为：{}", secret_number);
 
-    println!("请输入你猜的数字：");
+    loop {
+        println!("请输入你猜的数字：");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("没有读到任何内容");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("没有读到任何内容");
 
-    let guess: u32 = guess.trim().parse().expect("请输入数字！");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("你猜的数字是：{}", guess);
+        println!("你猜的数字是：{}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("猜小了！"),
-        Ordering::Greater => println!("猜大了！"),
-        Ordering::Equal => println!("你赢了！"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("猜小了！"),
+            Ordering::Greater => println!("猜大了！"),
+            Ordering::Equal => {
+                println!("你赢了！");
+                break;
+            },
+        }
     }
 }
